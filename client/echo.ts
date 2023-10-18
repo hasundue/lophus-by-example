a466b6fdd64d5c5b773ca7519a0771b244f60529
hasundue <hasundue@gmail.com>
2023-10-18 09:03:38 +0900

    docs(client/echo): use TextNoteComposer

--- client/echo.ts
+++ client/echo.ts
@@ -2,6 +2,7 @@
 import { Relay } from "$lophus/core/relays.ts?nips=1";
 import { Transformer } from "$lophus/lib/streams.ts";
 import { EventInit, EventPublisher } from "$lophus/lib/events.ts";
+import { TextNoteComposer } from "$lophus/lib/notes.ts";
 import { Signer } from "$lophus/lib/signs.ts";
 import { env } from "$lophus/lib/env.ts";
 
@@ -9,9 +10,12 @@ const relay = new Relay("wss://nos.lol");
 
 relay.subscribe({ kinds: [1], "#p": [env.PUBLIC_KEY] })
   .pipeThrough(
-    new Transformer((
-      ev,
-    ) => ({ kind: 1, content: ev.content } satisfies EventInit<1>)),
+    new Transformer((ev) =>
+      new TextNoteComposer().compose(
+        { kind: 1, content: ev.content } satisfies EventInit<1>,
+        { replyTo: ev },
+      )
+    ),
   )
   .pipeThrough(new EventPublisher(new Signer(env.PRIVATE_KEY)))
   .pipeTo(new Relay("wss://nos.lol"));
